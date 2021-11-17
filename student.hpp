902be4ba83657ac5f3f3194bd514b26a5323a9e1
Aleciomaia1 <86050364+Aleciomaia1@users.noreply.github.com>
2021-11-17 14:59:09 -0300

    Update student.hpp

--- student.hpp
+++ student.hpp
@@ -13,11 +13,21 @@ int mul(int a, int b);
 
 int add(int a, int b)
 {
+    int a;
+    int b;
+    SOMA=a+b
+    SOMA(b,a);  // Chamada da função SOMA();
    return 0; // TODO: fix
 }
 
 // Exercise 2: implements function 'mul', that multiplies two numbers
 int mul(int a, int b)
 {
+    int resultado;
+    int a;
+    int b;
+  
+  resultado = multiplica(V1,V2);
+  return 0;
    return 0; // TODO: fix
 }
